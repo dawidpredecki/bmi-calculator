7a38c479768c07ca8acb5bf41caea1c14eab27dd
Dawid <predecki.dawid@gmail.com>
2023-07-14 20:40:00 +0200

    Javascript

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-let weightForm = document.getElementById("weight");
-let heightForm = document.getElementById("height");
-let submitButton = document.getElementById("calculate-bmi");
+let bmiCalculator = document.forms["bmi_calculator"];
+let mainSection = document.getElementById("main_section");
+
+bmiCalculator.addEventListener("submit", calculateBMI);
+
+function calculateBMI(e) {
+    e.preventDefault();
+    let weight = parseFloat(this.weight.value.replace(",", "."));
+    let height = parseFloat(this.height.value.replace(",", "."));
+    let bmi = weight / (Math.pow(height, 2));
+    
+    let result = document.createElement("h3");
+    
+    result.textContent = `Twoje BMI: ${bmi.toFixed(2)}`;
+    mainSection.appendChild(result);
+}
+
+
+/* 
+let bmiValue = weightFormValue / (Math.pow(heightFormValue, 2));
+alert("Your BMI is " + bmiValue.toFixed(2));
+*/
